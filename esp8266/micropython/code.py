ba28bf46895512db37911d37b0fea3236bb36eca
Ernesto Montada Romero <e.montada0@gmail.com>
2022-11-27 12:35:19 +0100

    test code

--- esp8266/micropython/code.py
+++ esp8266/micropython/code.py
@@ -6,6 +6,8 @@ import time
 p1 = 0
 p2 = 0
 p3 = 0
+
+
 def printp():
     global p1
     global p2
@@ -13,11 +15,12 @@ def printp():
     p1 = rand(0, 1023)
     p2 = rand(0, 1023)
     p3 = rand(0, 1023)
-    par = [p1, p2, p3]
-    return par.join('|')
+    return "{}|{}|{}\r\n".format(p1, p2, p3).encode()
+
 
 def rand(a, b):
     return random.randint(a, b)
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 8089))
@@ -28,10 +31,13 @@ while True:
         gc.collect()
     try:
         conn, addr = s.accept()
+        conn.settimeout(None)
+        print('Got a new connection from {}'.format(str(addr)))
         while True:
-            conn.settimeout(None)
-            conn.sendall(printp())
+            conn.send(printp())
             time.sleep(1)
     except OSError as e:
         conn.close()
         print('Connection closed')
+    except Exception as e:
+        print('Error: {}'.format(e))
