a90e3155a912bc5c4311fcb5847f72d1615d97e9
Ernesto Montada Romero <e.montada0@gmail.com>
2022-11-30 20:52:56 +0100

    log NConnectionInfo

--- pkg/deej/config.go
+++ pkg/deej/config.go
@@ -166,6 +166,7 @@ func (cc *CanonicalConfig) Load() error {
 	cc.logger.Infow("Config values",
 		"sliderMapping", cc.SliderMapping,
 		"connectionInfo", cc.ConnectionInfo,
+		"NetworkConnectionInfo", cc.NConnectionInfo,
 		"invertSliders", cc.InvertSliders)
 
 	return nil
